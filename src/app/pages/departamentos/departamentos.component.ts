a3f8edbf838917212c57929f982eb7dec9a96cc8
GildoSavior <gildosalva.pe@gmail.com>
2025-03-16 14:41:36 +0100

    Create departament html

---
+++ src/app/pages/departamentos/departamentos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-departamentos',
+  imports: [],
+  templateUrl: './departamentos.component.html',
+  styleUrl: './departamentos.component.scss'
+})
+export class DepartamentosComponent {
+  
+
+}
