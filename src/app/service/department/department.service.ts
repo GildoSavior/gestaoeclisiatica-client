a29d6e9928fd4b0fb6df6a7c9caf5a8781b05923
GildoSavior <gildosalva.pe@gmail.com>
2025-03-17 21:25:03 +0100

    Create department service

--- src/app/service/department/department.service.ts
+++ src/app/service/department/department.service.ts
@@ -1,16 +1,47 @@
 import { Injectable } from '@angular/core';
 import { Department } from '../../models/departament.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { UserUtil } from '../user/userUtils';
+import { Observable } from 'rxjs';
+
+interface ApiResponse<T> {
+    message: string;
+    data: T;
+}
 
 @Injectable({
-  providedIn: 'root' // Isso garante que o serviço seja injetável globalmente
+    providedIn: 'root'
 })
-
 export class DepartmentService {
-    createDepartment(department: Department) {
-        throw new Error('Method not implemented.');
+    private readonly baseUrl = 'http://localhost:8080/api/departments';
+
+    constructor(private readonly http: HttpClient) {}
+
+    private getHeaders(): HttpHeaders {
+        const token = UserUtil.getUserData()?.jwtToken;
+        return new HttpHeaders({
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`
+        });
     }
-    updateDepartment(code: string) {
-        throw new Error('Method not implemented.');
+
+    getAll(): Observable<ApiResponse<Department[]>> {
+        return this.http.get<ApiResponse<Department[]>>(this.baseUrl, { headers: this.getHeaders() });
     }
-    constructor() { }
+
+    getDepartmentByCode(code: string): Observable<ApiResponse<Department>> {
+        return this.http.get<ApiResponse<Department>>(`${this.baseUrl}/${code}`, { headers: this.getHeaders() });
+    }
+
+    createDepartment(department: Department): Observable<ApiResponse<Department>> {
+        return this.http.post<ApiResponse<Department>>(this.baseUrl, department, { headers: this.getHeaders() });
+    }
+
+    updateDepartment(code: string, department: Department): Observable<ApiResponse<Department>> {
+        return this.http.put<ApiResponse<Department>>(`${this.baseUrl}/${code}`, department, { headers: this.getHeaders() });
+    }
+
+    deleteDepartment(code: string): Observable<ApiResponse<Department>> {
+        return this.http.delete<ApiResponse<Department>>(`${this.baseUrl}/${code}`, { headers: this.getHeaders() });
+    }
 }
