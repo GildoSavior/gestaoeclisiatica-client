5322efc327d624507a6decda29ffe68ff98d3ffc
GildoSavior <gildosalva.pe@gmail.com>
2025-03-25 22:08:53 +0100

    Create position service

--- src/app/service/position/position.service.ts
+++ src/app/service/position/position.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { UserUtil } from '../user/userUtils';
+import { Observable } from 'rxjs';
+import { Position } from '../../models/position.model';
+
+interface ApiResponse<T> {
+    message: string;
+    data: T;
+}
 
 @Injectable({
-  providedIn: 'root' // Isso garante que o serviço seja injetável globalmente
+    providedIn: 'root'
 })
 
 export class PositionService {
-    constructor() { }
+
+  private readonly baseUrl = 'http://localhost:8080/api/positions';
+
+    constructor(private readonly http: HttpClient) {}
+
+    private getHeaders(): HttpHeaders {
+        const token = UserUtil.getUserData()?.jwtToken;
+        return new HttpHeaders({
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`
+        });
+    }
+
+    getAll(): Observable<ApiResponse<Position[]>> {
+        return this.http.get<ApiResponse<Position[]>>(this.baseUrl, { headers: this.getHeaders() });
+    }
+
+    getPositionByCode(code: string): Observable<ApiResponse<Position>> {
+        return this.http.get<ApiResponse<Position>>(`${this.baseUrl}/${code}`, { headers: this.getHeaders() });
+    }
+
+    createPosition(position: Position): Observable<ApiResponse<Position>> {
+        return this.http.post<ApiResponse<Position>>(this.baseUrl, position, { headers: this.getHeaders() });
+    }
+
+    updatePosition(code: string, position: Position): Observable<ApiResponse<Position>> {
+        return this.http.put<ApiResponse<Position>>(`${this.baseUrl}/${code}`, position, { headers: this.getHeaders() });
+    }
+
+    deletePosition(code: string): Observable<ApiResponse<Position>> {
+        return this.http.delete<ApiResponse<Position>>(`${this.baseUrl}/${code}`, { headers: this.getHeaders() });
+    }
 }
