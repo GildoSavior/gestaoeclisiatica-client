7287c1495c07ee9ba960d532501729e01c258ef6
GildoSavior <gildosalva.pe@gmail.com>
2025-04-05 10:48:33 +0100

    Event details component with upload image

--- src/app/pages/events/components/event-details/event-details.component.ts
+++ src/app/pages/events/components/event-details/event-details.component.ts
@@ -8,15 +8,18 @@ import { ToastModule } from 'primeng/toast';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { CommonModule } from '@angular/common';
 import { Enum_EventType, EventStatus } from '../../../../models/enums/enums';
-
+import { MessageService } from 'primeng/api';
+import { FileUploadModule } from 'primeng/fileupload';
 
 @Component({
     selector: 'app-event-details',
     templateUrl: './event-details.component.html',
-    imports: [DialogModule, FormsModule, DropdownModule, ButtonModule, ToastModule, ProgressSpinnerModule, CommonModule],
+    imports: [DialogModule, FormsModule, DropdownModule, ButtonModule, ToastModule, ProgressSpinnerModule, CommonModule, FileUploadModule],
     standalone: true
 })
 export class EventDetailsComponent {
+    constructor(private messageService: MessageService) {}
+
     @Input() event: EventModel = {} as EventModel;
     @Input() visible: boolean = false;
     @Input() submitted: boolean = false;
@@ -26,16 +29,18 @@ export class EventDetailsComponent {
     @Output() save = new EventEmitter<void>();
 
     eventStatusOptions = Object.entries(EventStatus).map(([key, value]) => ({
-        name: value, 
-        value: key 
+        name: value,
+        value: key
     }));
     eventStatus: EventStatus | null = null;
 
     eventTypeOptions = Object.entries(Enum_EventType).map(([key, value]) => ({
-        name: value, 
-        value: key 
+        name: value,
+        value: key
     }));
     eventType: Enum_EventType | null = null;
+
+    uploadedFiles: any[] = [];
 
     hideDialog() {
         this.visibleChange.emit(false);
@@ -43,5 +48,13 @@ export class EventDetailsComponent {
 
     onSave(event: EventModel) {
         this.save.emit();
+    }
+
+    onUpload(event: any) {
+        for (const file of event.files) {
+            this.uploadedFiles.push(file);
+        }
+
+        this.messageService.add({ severity: 'info', summary: 'Success', detail: 'File Uploaded' });
     }
 }
