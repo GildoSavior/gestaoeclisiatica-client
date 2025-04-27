0ee7ded1345d30c5fa4d911b8fef798e0988568b
GildoSavior <gildosalva.pe@gmail.com>
2025-04-27 15:09:49 +0100

    Add client area

---
+++ src/app/pages/client-landing/client-landing.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-landing',
+  imports: [],
+  templateUrl: './client-landing.component.html',
+  styleUrl: './client-landing.component.scss'
+})
+export class ClientLandingComponent {
+
+}
--- src/app/pages/landing/landing.ts
+++ src/app/pages/landing/landing.ts
@@ -10,11 +10,18 @@ import { FeaturesWidget } from './components/featureswidget';
 import { HighlightsWidget } from './components/highlightswidget';
 import { PricingWidget } from './components/pricingwidget';
 import { FooterWidget } from './components/footerwidget';
+import { CarouselModule } from 'primeng/carousel';
 
 @Component({
     selector: 'app-landing',
     standalone: true,
-    imports: [RouterModule, TopbarWidget, HeroWidget, FeaturesWidget, HighlightsWidget, PricingWidget, FooterWidget, RippleModule, StyleClassModule, ButtonModule, DividerModule],
+    imports: [RouterModule, TopbarWidget, HeroWidget, FeaturesWidget, HighlightsWidget, PricingWidget, FooterWidget, RippleModule, StyleClassModule, ButtonModule, DividerModule, CarouselModule],
     templateUrl: 'landing.html',
 })
-export class Landing {}
+export class Landing {
+    imagens = [
+        { src: 'assets/carousel-images/image-1.jpeg', alt: 'Igreja 1' },
+        { src: 'assets/carousel-images/image-3.jpg', alt: 'Igreja 2' },
+        { src: 'assets/carousel-images/image-2.jpg', alt: 'Igreja 3' },
+      ];
+}
