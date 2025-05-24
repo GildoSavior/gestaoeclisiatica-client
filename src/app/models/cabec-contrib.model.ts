30073bc5906b8c9b2f18e88fb14ccfd42a2b039f
GildoSavior <gildosalva.pe@gmail.com>
2025-05-24 11:35:44 +0100

    Add contrib model

--- src/app/models/cabec-contrib.model.ts
+++ src/app/models/cabec-contrib.model.ts
@@ -2,14 +2,17 @@ import { LineContrib } from "./line-contrib.model";
 import { EventModel } from "./event.model";
 import { ContribStatus } from "./enums/enums";
 
+
 export interface CabecContrib {
-  id: string;
+  id: number;
+  title: string;
+  type: string; // Ajuste os valores conforme o enum ContribType
   description: string;
   total: number;
-  dateTime: Date;
-  createdAt: Date;
-  updatedAt: Date;
-  lineContrib: LineContrib;
-  event: EventModel | null;
-  cabecStatus: ContribStatus;
+  totalApproved: number;
+  createdAt: string; // ISO 8601 format (ex: '2025-05-24T12:34:56')
+  updatedAt: string;
+  event_id: number;
+  eventCode: string;
+  cabecStatus: string;
 }
